301a8217603433d10b3f0b2c1d0fc65fa9ffb482
CHARITAH <102732524+CHARITAH@users.noreply.github.com>
2022-11-22 19:36:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ var last_position_of_x , last_position_of_y ;
 color = "purple";
 linewidth = 2 ;
 
+last_position_of_x = current_position_of_mouse_x; 
+last_position_of_y = current_position_of_mouse_y;
+var last_position_of_touch_x, last_position_of_touch_y;
+var width = screen.width;
+new_width = screen.width - 70; 
+new_height = screen.height - 300;
+if(width < 992) { 
+    document.getElementById("myCanvas").width = new_width;
+    document.getElementById("myCanvas").height = new_height;
+    document.body.style.overflow = "hidden"; }
+
 canvas.addEventListener ("mouseup", up);
 function up(e) {
     mouseevent = "mouseup";
@@ -86,14 +97,5 @@ canvas.addEventListener("touchmove", my_touchmove);
         ctx.stroke();
         
 
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
-        var last_position_of_touch_x, last_position_of_touch_y;
-        var width = screen.width;
-        new_width = screen.width - 70; 
-        new_height = screen.height - 300;
-        if(width < 992) { 
-            document.getElementById("myCanvas").width = new_width;
-            document.getElementById("myCanvas").height = new_height;
-            document.body.style.overflow = "hidden"; }
+       
     }
